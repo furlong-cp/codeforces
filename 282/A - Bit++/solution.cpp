f38a8b6c2ffaa69b9c0789b47224373500513424
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-09 11:16:16 +0530

    Add Bit++ [A] from Codeforces

--- 282/A - Bit++/solution.cpp
+++ 282/A - Bit++/solution.cpp
@@ -2,11 +2,14 @@
 using namespace std;
  
 int main (){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int n;
     cin >> n ; 
     string s;
     int sum = 0;
-    for (int i = 0; i < n; i++){
+    while (n--){
         cin >> s;
         if (s[0] == '+' ||s[2] == '+'){
             sum ++;}
