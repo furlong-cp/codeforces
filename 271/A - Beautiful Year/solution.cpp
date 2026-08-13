0f03f69ce5aaee960b1ced2873364da404220b98
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-13 20:52:09 +0530

    Add Beautiful Year [A] from Codeforces

---
+++ 271/A - Beautiful Year/solution.cpp
@@ -0,0 +1,31 @@
+/*
+author - furlong
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int y;
+    cin >> y;
+ 
+    while (true) {
+        y++;
+ 
+        int a = y / 1000;
+        int b = y / 100 % 10;
+        int c = y / 10 % 10;
+        int d = y % 10;
+ 
+        if (a != b && a != c && a != d &&
+            b != c && b != d &&
+            c != d) {
+            cout << y;
+            break;
+     
+        }
+    }
+ 
+    return 0;
+}
