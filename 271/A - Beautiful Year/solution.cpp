88e09687fcc7fd915d18b2387243d67a41297327
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-13 20:53:34 +0530

    Add Beautiful Year [A] from Codeforces

--- 271/A - Beautiful Year/solution.cpp
+++ 271/A - Beautiful Year/solution.cpp
@@ -7,20 +7,17 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int y;
-    cin >> y;
+    int y; // year
+    cin >> y; // input year
  
     while (true) {
         y++;
+        int a = y / 1000; // check thousand-th digit
+        int b = y / 100 % 10; // check hundred-th digit
+        int c = y / 10 % 10; // check ten-th digit
+        int d = y % 10; // check ones digit
  
-        int a = y / 1000;
-        int b = y / 100 % 10;
-        int c = y / 10 % 10;
-        int d = y % 10;
- 
-        if (a != b && a != c && a != d &&
-            b != c && b != d &&
-            c != d) {
+        if (a != b && a != c && a != d && b != c && b != d && c != d) {
             cout << y;
             break;
      
