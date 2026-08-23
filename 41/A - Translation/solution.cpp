f8d2f708699d1c6b2984a6737e4d21742c29f51e
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-23 14:11:18 +0530

    Add Translation [A] from Codeforces

---
+++ 41/A - Translation/solution.cpp
@@ -0,0 +1,44 @@
+/*#include <bits/stdc++.h>
+using namespace std ; 
+ 
+int main (){
+    string a,b;
+    cin >> a ; 
+    cin >> b ; 
+    bool found = true ; 
+    for (int i = 0 ; i < a.size() ; i++){
+        if (a[i]==b[a.size()-i-1]){
+            found = true ; 
+        }
+        else {
+            found = false ; 
+            break ; 
+        }
+    }
+    if (found){
+        cout << "YES" ;
+    }
+    else {
+        cout << "NO" ; 
+    }
+}
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0) ; 
+    
+    string a, b;
+    cin >> a >> b;
+ 
+    reverse(a.begin(), a.end());
+ 
+    if (a == b)
+        cout << "YES";
+    else
+        cout << "NO";
+ 
+    return 0;
+}
