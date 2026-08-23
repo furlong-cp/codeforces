9255ddafb39e65c04d73d9f32095ff0637407def
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-23 12:23:45 +0530

    Add Anton and Danik [A] from Codeforces

---
+++ 734/A - Anton and Danik/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std ;
+ 
+int main (){
+    int n;
+    cin >> n ;
+    string s;
+    cin >>s ;
+    int A=0;
+    int D=0 ;
+    for (int i = 0 ; i < n ; i++){
+        if (s[i]=='A')A++;
+        else D++;    
+    }
+    if(A>D){
+        cout << "Anton" ;
+    }
+    else if (A<D){
+        cout << "Danik" ;
+    }
+    else {
+        cout << "Friendship" ;
+    }
+}
