c28a0c98806e6d28872e76fea8c681e96706d853
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-23 14:53:50 +0530

    Add Magnets [A] from Codeforces

---
+++ 344/A - Magnets/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int n;
+    cin >> n;
+ 
+    vector<string> v(n);
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+ 
+    int count = 1;
+ 
+    for (int i = 1; i < n; i++) {
+        if (v[i] != v[i - 1]) {
+            count++;
+        }
+    }
+ 
+    cout << count << '
+';
+ 
+    return 0;
+}
