294e25895dab5c838d83369ba47fa987abcadbff
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-26 10:41:17 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main (){
+    ios::sync_with_stdio(0);
+    cin.tie(0) ;
+    int m , n ; 
+    cin >> m >> n ; 
+    if (m==n){
+        cout << (m*n) / 2*1 ;
+    }
+    else {
+        cout << ((m*n)/2 *1);
+    }
+ 
+}
