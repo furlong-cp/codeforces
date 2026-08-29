705c74d75272494cc396541228be1b5751c14a3f
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-29 12:05:06 +0530

    Add Vasya the Hipster [A] from Codeforces

---
+++ 581/A - Vasya the Hipster/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int a, b;
+    cin >> a >> b;
+ 
+    int diff = 0;
+    int same = 0;
+ 
+    while (a + b > 0) {
+        if (a >= 1 && b >= 1) {
+            diff++;
+            a--;
+            b--;
+        }
+        else if (a >= 2) {
+            same++;
+            a -= 2;
+        }
+        else if (b >= 2) {
+            same++;
+            b -= 2;
+        }
+        else {
+            break;
+        }
+    }
+ 
+    cout << diff << " " << same;
+}
