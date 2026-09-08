6c2c496b0303a8dba78ed7c4eccec17dabc59e58
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-09-08 19:02:01 +0530

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+ 
+int main (){
+    ios::sync_with_stdio(0);
+    cin.tie(0) ;
+    
+    
+    unsigned short int t ; // test cases
+    cin>> t ; 
+    while (t--){
+        unsigned short int a ,b ; 
+        cin >> a >> b  ;
+        cout << b-a << "
+"  ; 
+    }
+}
