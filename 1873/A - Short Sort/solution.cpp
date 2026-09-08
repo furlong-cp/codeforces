0a35138a66e16b6b6dad71d5f783033e60f195cd
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-09-08 19:05:45 +0530

    Add Short Sort [A] from Codeforces

---
+++ 1873/A - Short Sort/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+ 
+int main (){
+    ios::sync_with_stdio(0);
+    cin.tie(0) ; 
+    
+    int t ; // test cases 
+    cin >> t ;
+    
+    while (t--){
+        string s  ; 
+        cin >> s  ;
+        if (s[0]=='a' || s[1]=='b' || s[2]=='c'){
+            cout << "YES
+" ; 
+        }
+        else cout << "NO
+" ; 
+    }
+    
+    
+    
+}
